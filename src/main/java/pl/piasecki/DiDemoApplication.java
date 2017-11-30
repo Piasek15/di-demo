c22831d1523051a7baf856a99cb8f6cf279c7687
Patrick <piasecki.patrick.15@gmail.com>
2017-11-30 12:26:56 +0100

    create MyController

--- src/main/java/pl/piasecki/DiDemoApplication.java
+++ src/main/java/pl/piasecki/DiDemoApplication.java
@@ -1,12 +1,22 @@
 package pl.piasecki;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import pl.piasecki.controllers.MyController;
+
+import java.applet.AppletContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		controller.hello();
 	}
 }
---
+++ src/main/java/pl/piasecki/controllers/MyController.java
@@ -0,0 +1,14 @@
+package pl.piasecki.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String hello(){
+        System.out.println("Hello!!!");
+
+        return "foo";
+    }
+
+}
